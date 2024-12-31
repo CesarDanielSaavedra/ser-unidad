ac4d478c902e97765e76f0f9ab34b6831c795772
Daniel Saavedra <cesardsaavedra@gmai.com>
2024-12-31 12:03:14 -0300

    deployment was configured in github-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ser-unidad/', // Cambia 'nombre-repo' por el nombre de tu repositorio
   plugins: [react()],
 })
