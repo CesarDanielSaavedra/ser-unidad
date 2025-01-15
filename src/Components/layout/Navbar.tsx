ffff6e96cc72481ccd62b74e8af989ae436986a4
Daniel Saavedra <cesardsaavedra@gmai.com>
2025-01-15 15:29:01 -0300

    Changed the global configuration to include the base URL. The navbar has been modified so that the links in the navbar collapse into a hamburger menu when the application is used on small devices.

--- src/Components/layout/Navbar.tsx
+++ src/Components/layout/Navbar.tsx
@@ -1,6 +1,6 @@
-//import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { useState } from 'react';
 import { useLanguage } from '../../hooks/useLanguage';
 import { useDictionary } from '../../hooks/useDictionary';  
 
@@ -11,28 +11,54 @@ const Navbar = () => {
   const { language, setLanguage } = useLanguage();
   const { components } = useDictionary(language);  
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleLanguageChange = () => {
+    setLanguage(
+      language === components.ToogleButton.lenguageButton.initValue
+        ? components.ToogleButton.lenguageButton.alterValue
+        : components.ToogleButton.lenguageButton.initValue
+    );
+  };
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
     <nav className="bg-blue-500 p-4 text-white">
-      <ul className="flex align-center space-x-4">
-        <li><Link to="/">Inicio</Link></li>
-        <li><Link to="/about">Acerca de Sergio</Link></li>
-        <li><Link to="/yoga-classes">Yoga</Link></li>
-        <li><Link to="/meditation">Meditación</Link></li>
-        <li><Link to="/custom-classes">Personalizadas</Link></li>
-        <li><Link to="/blog">Blog</Link></li>
-        <li><Link to="/contact">Contacto</Link></li>
-        <li><Link to="/philosophy">Filosofía</Link></li>
-        <li><Link to="/practice-space">Práctica</Link></li>
-      </ul>
-      <ToggleButton 
-        initValue={components.ToogleButton.lenguageButton.initValue} 
-        alterValue={components.ToogleButton.lenguageButton.alterValue}
-        onClick={()=>setLanguage(
-          language === components.ToogleButton.lenguageButton.initValue ? 
-          components.ToogleButton.lenguageButton.alterValue : 
-          components.ToogleButton.lenguageButton.initValue
-        )}
-      />
+      <div className="flex justify-between items-center">
+
+        <button
+          onClick={toggleMenu}
+          className={`block lg:hidden p-2 bg-blue-500 text-white focus:outline-none transition-transform duration-200 ${isMenuOpen ? 'rotate-90' : ''}`}
+        >
+          <div className="w-6 h-0.5 bg-white mb-1"></div>
+          <div className="w-6 h-0.5 bg-white mb-1"></div>
+          <div className="w-6 h-0.5 bg-white"></div>
+        </button>
+
+        <ul
+            className={`lg:flex lg:space-x-4 transition-all duration-300 ease-in-out ${
+              isMenuOpen ? 'block' : 'hidden lg:block'
+            }`}
+          >
+          <li><Link to="/">Inicio</Link></li>
+          <li><Link to="/about">Acerca de Sergio</Link></li>
+          <li><Link to="/yoga-classes">Yoga</Link></li>
+          <li><Link to="/meditation">Meditación</Link></li>
+          <li><Link to="/custom-classes">Personalizadas</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
+          <li><Link to="/contact">Contacto</Link></li>
+          <li><Link to="/philosophy">Filosofía</Link></li>
+          <li><Link to="/practice-space">Práctica</Link></li>
+        </ul>
+
+        <ToggleButton 
+          initValue={components.ToogleButton.lenguageButton.initValue} 
+          alterValue={components.ToogleButton.lenguageButton.alterValue}
+          onClick={handleLanguageChange}
+        />
+        
+      </div>
     </nav>
   );
 };
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly MODE: string; // "development" | "production"
+    readonly BASE_URL: string; // Base URL de Vite
+    readonly VITE_SOME_KEY?: string; // Ejemplo para variables personalizadas
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
